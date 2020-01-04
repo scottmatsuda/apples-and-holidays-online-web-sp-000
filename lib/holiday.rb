d108bcbc09e123f1ad26c3e67f27c5931d19702c
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-04 18:07:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,10 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_hash[season].each do |holiday, event|
       #   puts "  #{holiday.to_s.split("_").join(" ").capitalize}: #{event.join(", ")}"
       # end
-      
+      capitalized = holiday.to_s.split("_").map do |stuff|
+        stuff.capitalize
+      end
+      puts "#{capitalized.join(" ")}: #{event.join(", ")}"
   end
 end
 
